fed8fc887f89be7d9bc169394ea18c672abf2892
Lenin2611 <leninsantiago2611@gmail.com>
2023-10-20 13:40:29 -0500

    feat: :sparkles: Made a second migration

---
+++ Infrastructure/Data/Migrations/20231020183712_SecondMig.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class SecondMig : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "IdEmpleado",
+                table: "empleado",
+                newName: "IdEmp");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_empleado_IdEmpleado",
+                table: "empleado",
+                newName: "IX_empleado_IdEmp");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "IdEmp",
+                table: "empleado",
+                newName: "IdEmpleado");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_empleado_IdEmp",
+                table: "empleado",
+                newName: "IX_empleado_IdEmpleado");
+        }
+    }
+}
